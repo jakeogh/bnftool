7a31148f95d55685f9af91d99c93532309752580
jakeogh <github.com@v6y.net>
2022-06-24 09:40:44 -0700

    auto-commit

--- bnftool/bnftool.py
+++ bnftool/bnftool.py
@@ -19,142 +19,75 @@
 # pylint: disable=E1101  # no member for base
 # pylint: disable=W0201  # attribute defined outside __init__
 # pylint: disable=R0916  # Too many boolean expressions in if statement
-# pylint: disable=C0305  # Trailing newlines editor should fix automatically, pointless warning
 
-import os
-import sys
-import time
-from pathlib import Path
+# import os
+# import sys
+# import time
+# from pathlib import Path
 from signal import SIG_DFL
 from signal import SIGPIPE
 from signal import signal
-from typing import Iterable
-from typing import Optional
-from typing import Sequence
+# from typing import Iterable
+# from typing import Optional
+# from typing import Sequence
 from typing import Union
 
 import click
-from asserttool import ic
-from asserttool import validate_slice
+# from asserttool import ic
 from click_auto_help import AHGroup
 from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tv
-from eprint import eprint
 from mptool import output
-from mptool import unmp
-from retry_on_exception import retry_on_exception
-#from mptool import unmp
-#from prettyprinter import cpprint
-#from prettyprinter import install_extras
-#install_extras(['attrs'])
-from timetool import get_timestamp
-
-#from prettytable import PrettyTable
-#output_table = PrettyTable()
-
-# click-command-tree
-#from click_plugins import with_plugins
-#from pkg_resources import iter_entry_points
 
 signal(SIGPIPE, SIG_DFL)
 
 
 def get_bnf_syntax():
     BNF_syntax = {
-        '< >': 'Defined element',
-        '=': 'is defined as',
-        '|': 'exclusive OR',
-        '{ }': 'Group; one element is required',
-        '[ ]': 'Optional; can be omitted',
-        '. . .': 'Previous element(s) may be repeatsd',
-        '( )': 'Comment',
-        }
+        "< >": "Defined element",
+        "=": "is defined as",
+        "|": "exclusive OR",
+        "{ }": "Group; one element is required",
+        "[ ]": "Optional; can be omitted",
+        ". . .": "Previous element(s) may be repeatsd",
+        "( )": "Comment",
+    }
     return BNF_syntax
 
 
-#@with_plugins(iter_entry_points('click_command_tree'))
 @click.group(no_args_is_help=True, cls=AHGroup)
 @click_add_options(click_global_options)
 @click.pass_context
-def cli(ctx,
-        verbose: Union[bool, int, float],
-        verbose_inf: bool,
-        ) -> None:
+def cli(
+    ctx,
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+) -> None:
 
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
 
 @cli.command()
 @click_add_options(click_global_options)
 @click.pass_context
-def syntax(ctx,
-           verbose: Union[bool, int, float],
-           verbose_inf: bool,
-           ) -> None:
+def syntax(
+    ctx,
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+    dict_input: bool,
+) -> None:
 
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
-    output(get_bnf_syntax(), tty=tty, verbose=verbose)
-
-
-#@cli.command()
-#@click.argument("paths", type=str, nargs=-1)
-#@click.argument("sysskel",
-#                type=click.Path(exists=False,
-#                                dir_okay=True,
-#                                file_okay=False,
-#                                allow_dash=False,
-#                                path_type=Path,),
-#                nargs=1,
-#                required=True,)
-#@click.option('--ipython', is_flag=True)
-#@click_add_options(click_global_options)
-#@click.pass_context
-#def cli(ctx,
-#        paths: Sequence[str],
-#        sysskel: Path,
-#        ipython: bool,
-#        verbose: Union[bool, int, float],
-#        verbose_inf: bool,
-#        ) -> None:
-#
-#    tty, verbose = tv(ctx=ctx,
-#                      verbose=verbose,
-#                      verbose_inf=verbose_inf,
-#                      )
-#
-#    if paths:
-#        iterator = paths
-#    else:
-#        iterator = unmp(valid_types=[bytes,], verbose=verbose)
-#    del paths
-#
-#    index = 0
-#    for index, path in enumerate(iterator):
-#        path = Path(os.fsdecode(path))
-#
-#        if verbose:  # or simulate:
-#            ic(index, path)
-#        #if count:
-#        #    if count > (index + 1):
-#        #        ic(count)
-#        #        sys.exit(0)
-#
-#        with open(path, 'rb') as fh:
-#            path_bytes_data = fh.read()
-#
-#        if not count:
-#            output(path, tty=tty, verbose=verbose)
-#
-#    if count:
-#        output(index + 1, tty=tty, verbose=verbose)
-#
-##        if ipython:
-##            import IPython; IPython.embed()
+    output(
+        get_bnf_syntax(), reason=None, tty=tty, dict_input=dict_input, verbose=verbose
+    )
